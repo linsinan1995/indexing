339be4e2d5f910314bdf4356d72e7e84dabee745
linsinan1995 <mynameisxiaou@gmail.com>
2020-02-22 23:30:56 +0100

    improve lab1 & complete first task in lab2

--- lab2.py
+++ lab2.py
@@ -1,24 +1,23 @@
 '''
-Filename: e:\indexing\lab1.py
+Filename: e:\indexing\lab2.py
 Path: e:\indexing
-Created Date: Saturday, February 22nd 2020, 2:35:04 pm
+Created Date: Saturday, February 22nd 2020, 4:59:55 pm
 Author: linsinan1995
 
-Copyright (c) 2020
-
-Image Retrieval(Near Duplicate)
+Copyright (c) 2020 
+Image Retrieval (from duplicate to categories)
 '''
+
 
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import os
-print(cv2.__version__)
 
 
 class imageQuerier:
     def __init__(self, images):
-        self.images = {i : cv2.imread(images[i],1) for i in range(len(images)) }
+        self.images = {i : cv2.resize(cv2.imread(images[i],1), (224, 224)) for i in range(len(images)) }
         self.query_image = None 
         self.size = len(images)
 
@@ -58,9 +57,14 @@ class imageQuerier:
         gray2 = cv2.cvtColor(image_two, cv2.COLOR_BGR2GRAY)
         sift = cv2.xfeatures2d.SIFT_create()
         # find the keypoints and descriptors with SIFT
-        kp1, des1 = sift.detectAndCompute(gray,None)
-        kp2, des2 = sift.detectAndCompute(gray2,None)
-
+        kp1, des1 = sift.detectAndCompute(gray, None)
+        kp2, des2 = sift.detectAndCompute(gray2, None)
+        
+        if type(des1) == type(None) or len(des1) <= 2:
+            print("Entered Picture doesnt have enough key point")
+            return 0
+        if type(des2) == type(None) or len(des2) <= 2:
+            return 0
         # FLANN parameters
         FLANN_INDEX_KDTREE = 0
         index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 2)
@@ -89,14 +93,25 @@ class imageQuerier:
         scores = {i:0 for i in range(len(self.images))}
         for i in range(self.size):
             score = self.match(index = i, factor = factor, draw = False, query = True)
-            print("{}, {}".format(i, score))
+            # print("{}, {}".format(i, score))
             scores[i] = score
-        top_similar_pic_index = sorted(scores,  key=scores.get, reverse = True)[:4]
+        top_similar_pic_index = sorted(scores,  key=scores.get, reverse = True)
 
-        total_score = sum([scores[idx] for idx in top_similar_pic_index])
-        top_similar_pic = {idx:scores[idx]/total_score for idx in top_similar_pic_index}
+        total_score = sum([scores[idx] for idx in top_similar_pic_index[:4]])
+        top_similar_pic = {idx:scores[idx]/total_score for idx in top_similar_pic_index[:4]}
         
-        return top_similar_pic
+        return top_similar_pic, top_similar_pic_index
+
+    def BOWquery(self, image):
+        self.query_image = image
+        # Initiate SIFT detector
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        sift = cv2.xfeatures2d.SIFT_create()
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(gray, None)
+        kp2, des2 = sift.detectAndCompute(gray2, None)
+        
 
     def plot_query_result(self, top_similar_pic, size = (20, 15)):
         plt.figure(figsize=size)
@@ -118,26 +133,43 @@ def random_index(_min, _max):
     np.random.seed(250)
     return np.random.randint(_min, _max)
 
+def load_data(data_directory):
+    directories = [d for d in os.listdir(data_directory) 
+                   if os.path.isdir(os.path.join(data_directory, d))]
+    labels = []
+    images = []
+    _dict = {int(label):[] for label in directories}
+    for d in directories:
+        label_directory = os.path.join(data_directory, d)
+        file_names = [os.path.join(label_directory, f) 
+                      for f in os.listdir(label_directory) ]
+        label = int(d)
+        for filename in file_names:
+            images.append(filename)
+            labels.append(label)
+            _dict[label].append(filename)
+    return images, labels, _dict
+
 if __name__ == "__main__":
+    ROOT_PATH = "data"
+    train_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Train")
 
-    img = imageQuerier(["data/MarsBar1.jpg", "data/MarsBar2.jpg"])
-    img.display(0)
-    img.sift_kp_detect(draw=True)
-    img.match()
+    # load data
+    images_train, labels_train, label_dict = load_data(train_data_directory)
     
-    data_dir = "data/VOC2007/JPEGImages/"
-    # pick 30 images in this dir
-    n_pics = 30
-    data_paths = [data_dir + image  for _, _, images in os.walk(data_dir) for i, image in enumerate(images) if i < n_pics]
-    img_querier = imageQuerier(data_paths)
-
-    idx = random_index(0, len(data_paths))
-    img_affined = cv2.imread(data_paths[idx], 1)
-
-    M = np.float32([[1,0,100],[0,1,50]])
-    rows, cols, _ = img_affined.shape
-    img_affined = cv2.warpAffine(img_affined, M, (cols,rows))
-    img_querier.display(img=img_affined)
-
-    most_similar = img_querier.query(img_affined)
-    img_querier.plot_query_result(most_similar)
+    # stop => label: 14
+    # sample
+    iq = imageQuerier(images_train)
+    idx = random_index(0, len(label_dict[14]))
+    image = cv2.imread(label_dict[14][idx], 1)
+    
+    most_similar, similar_pic_index = iq.query(image)
+    iq.plot_query_result(most_similar)
+    
+    cnt = 0
+    for i, idx in enumerate(similar_pic_index):
+        if labels_train[idx] == 14:
+            cnt += 1
+            if cnt % 50 == 0 or cnt == 1:
+                print("Top {} similar images include {} Stop images, taking {:.4f}%".format(i+1, cnt, cnt/(i+1) * 100))
+            
